5550f6cd728c35af9f6e6e5ecec72c4d5b2b2c22
7moo3alealamiu <44821832+7moo3alealamiu@users.noreply.github.com>
2019-01-04 22:44:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1135,21 +1135,31 @@ client.on('message', message => {
     }
 });
 
-if(command === "clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-        msg.delete().then
-    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
-} else {
-    msg.delete().then
-    msg.delete().then
-    msg.channel.bulkDelete(textxt);
-        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
-        }    
-    }
-}
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","Members"));
+    });
+
+client.on("message", message => {
+    var prefix = "!";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('? | **ليس لديك صلاحيات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل بنجاح",
+        footer: {
+          text: "11111."
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
 });
 
 client.login(process.env.BOT_TOKEN);
