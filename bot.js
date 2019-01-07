98f347093d79658e0f5c62f935bd7a1441f19521
7moo3alealamiu <44821832+7moo3alealamiu@users.noreply.github.com>
2019-01-07 12:49:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const UserBlocked = new Set();
-const prefix = '^'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- 
- 
  
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : GMZN Host`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : @ GMZN Host ' `);
-    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setStatus("online")
- 
-});
-
-//كود تغير البلاينج او التويتش
-
-const adminprefix = "^";//تذكير نغير البرفكس
-const devs = ['516576049778130954','492575063233658880'];//zمهم نحط الايدي
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' '); //حقوق GMZN Host
-    if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'ply')) { //حقوق GMZN Host
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`) //حقوق GMZN Host
-} else
- 
-if (message.content.startsWith(adminprefix + 'tw')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`) //حقوق GMZN Host
-}
+client.user.setGame(`!~[ Čømmäñđš l ^help ]~!`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 //-------------------------------------------------------------نهاية السورس الاساسي--------------------------------------------------------------------------------------------------------------
