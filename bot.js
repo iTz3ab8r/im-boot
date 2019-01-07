e83411be65bf602865ebdc8c5a20e5644f74b9ca
7moo3alealamiu <44821832+7moo3alealamiu@users.noreply.github.com>
2019-01-07 13:00:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = '^'
+ 
 client.on('ready', () => {
-  client.user.setGame(`!~[ Čømmäñđš l ^help ]~!.`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
   console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot with GMZN Host')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 //-------------------------------------------------------------نهاية السورس الاساسي--------------------------------------------------------------------------------------------------------------
